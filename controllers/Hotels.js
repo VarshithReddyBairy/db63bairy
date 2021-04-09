ca0a77093782440ae56e98fce4c35758e6a52726
VarshithReddyBairy <77808640+VarshithReddyBairy@users.noreply.github.com>
2021-04-09 16:46:17 -0500

    commited for put update

--- controllers/Hotels.js
+++ controllers/Hotels.js
@@ -1,5 +1,6 @@
 var Hotels = require('../models/Hotels');
-// List of all Costumes
+//for a specific Costume.
+
 exports.Hotels_list = async function(req, res) {
     try{
         theHotels = await Hotels.find();
@@ -9,19 +10,27 @@ exports.Hotels_list = async function(req, res) {
         res.error(500,`{"error": ${err}}`);
     }
 };
-// for a specific Costume.
-exports.Hotels_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Hotels detail: ' + req.params.id);
-};
-// Handle Costume create on POST.
-// Handle Costume create on POST.
+
+// exports.Hotels_detail = function(req, res) {
+// res.send('NOT IMPLEMENTED: Hotels detail: ' + req.params.id);
+// };
+
+exports.Hotels_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id)  
+      try {      
+            result = await Hotels.findById( req.params.id)    
+                res.send(result)  
+               } 
+     catch (error) {  
+               res.status(500)  
+                res.send(`{"error": document for id ${req.params.id} not found`);   
+              }
+             };
+
 exports.Hotels_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Hotels();
-    // We are looking for a body, since POST does not have query parameters.
-    // Even though bodies can be in many different formats, we will be picky
-    // and require that it be a json object
-    // {"costumetype":"goat", "cost":12, "size":"large"}
+    
     document.roomType = req.body.costumetype;
     document.price = req.body.price;
     document.location = req.body.location;
@@ -33,17 +42,37 @@ exports.Hotels_create_post = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
-// Handle Costume delete form on DELETE.
+
 exports.Hotels_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Hotels delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
-exports.Hotels_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Hotels update PUT' + req.params.id);
-};
 
-// VIEWS
-// Handle a show all view
+// exports.Hotels_update_put = function(req, res) {
+// res.send('NOT IMPLEMENTED: Hotels update PUT' + req.params.id);
+// };
+
+exports.Hotels_update_put = async function(req, res) {  
+      console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)  
+        try {       
+             let toUpdate = await Hotels.findById( req.params.id)      
+               // Do updates of properties        
+               if(req.body.roomType) 
+                 toUpdate.roomType = req.body.roomType;      
+               if(req.body.price)
+                  toUpdate.price = req.body.price;      
+               if(req.body.location)
+                    toUpdate.location = req.body.location;      
+                     let result = await toUpdate.save();     
+                        console.log("Sucess " + result)   
+                             res.send(result)    
+                            }
+                 catch (err) {     
+                        res.status(500)   
+                             res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+                               }
+                            };
+
+
 exports.Hotels_view_all_Page = async function(req, res) {
     try{
         theHotels = await Hotels.find();
